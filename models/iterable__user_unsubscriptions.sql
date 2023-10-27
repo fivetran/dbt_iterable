81228dae98fbfde67c9ffffb6ef68015a70a36f6
fivetran-reneeli <renee.li@fivetran.com>
2023-10-27 17:02:38 -0400

    update user unsubscriptions model to include for non history

--- models/iterable__user_unsubscriptions.sql
+++ models/iterable__user_unsubscriptions.sql
@@ -7,7 +7,6 @@ with message_type_channel as (
 
     select
         *
-
     from {{ var('user_unsubscribed_channel') }}
 
 {% if var('iterable__using_user_unsubscribed_message_type', True) %}
@@ -15,10 +14,55 @@ with message_type_channel as (
 
     select 
         *
-
     from {{ var('user_unsubscribed_message_type') }}
 
 {% endif %}
+
+{% if does_table_exist('user_unsubscribed_channel') %}
+), combine as (
+
+    select 
+        _fivetran_user_id,
+        channel_id,
+        cast(null as {{ dbt.type_int() }}) as message_type_id
+    from user_unsubscribed_channel
+
+{% if var('iterable__using_user_unsubscribed_message_type', True) %}
+
+{% if does_table_exist('user_unsubscribed_message_type') %}
+    union all
+
+    select 
+        _fivetran_user_id,
+        cast(null as {{ dbt.type_int() }}) as channel_id,
+        message_type_id
+    from user_unsubscribed_message_type
+{% endif %}
+{% endif %}
+
+), final as (
+
+    select 
+        combine._fivetran_user_id,
+        -- coalescing since message_type -> channel goes up a grain
+        coalesce(combine.channel_id, message_type_channel.channel_id) as channel_id,
+        coalesce(combine.message_type_id, message_type_channel.message_type_id) as message_type_id,
+        message_type_channel.channel_name,
+        message_type_channel.message_type_name,
+        message_type_channel.channel_type,
+        message_type_channel.message_medium
+        case when combine.channel_id is not null then true else false end as is_unsubscribed_channel_wide
+
+    from combine
+
+    -- unsubscribing from an entire channel unsubscribes a user from all message types in that channel
+    join message_type_channel 
+        on combine.channel_id = message_type_channel.channel_id
+        or combine.message_type_id = message_type_channel.message_type_id
+)
+
+{% else %}
+
 ), combine_histories as (
 
 -- we are combining because channels are effectively parents of message types
@@ -63,6 +107,9 @@ with message_type_channel as (
         on combine_histories.channel_id = message_type_channel.channel_id
         or combine_histories.message_type_id = message_type_channel.message_type_id
 )
+
+{% endif %}
+{% endif %}
 
 select *
 from final
