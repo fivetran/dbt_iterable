08f454b3f08d94f96fdaf5f5d5632ce09dfa1b92
fivetran-reneeli <renee.li@fivetran.com>
2023-11-01 13:52:03 -0400

    update unsub model

--- models/iterable__user_unsubscriptions.sql
+++ models/iterable__user_unsubscriptions.sql
@@ -8,6 +8,7 @@ with message_type_channel as (
     select
         *
     from {{ var('user_unsubscribed_channel') }}
+    where latest_batch_index = 1
 
 {% if var('iterable__using_user_unsubscribed_message_type', True) %}
 ), user_unsubscribed_message_type as (
@@ -15,9 +16,10 @@ with message_type_channel as (
     select
         *
     from {{ var('user_unsubscribed_message_type') }}
+    where latest_batch_index = 1
+
 {% endif %}
 
-{% if does_table_exist('user_unsubscribed_channel') %}
 ), combine as (
 
     select 
@@ -29,7 +31,6 @@ with message_type_channel as (
 
 {% if var('iterable__using_user_unsubscribed_message_type', True) %}
 
-{% if does_table_exist('user_unsubscribed_message_type') %}
     union all
 
     select 
@@ -38,7 +39,6 @@ with message_type_channel as (
         cast(null as {{ dbt.type_string() }}) as channel_id,
         message_type_id
     from user_unsubscribed_message_type
-{% endif %}
 {% endif %}
 
 ), final as (
@@ -65,58 +65,3 @@ with message_type_channel as (
 
 select *
 from final
-
-{% else %}
-
-), combine_histories as (
-
--- we are combining because channels are effectively parents of message types
-    select 
-        email,
-        unique_user_key,
-        channel_id,
-        cast(null as {{ dbt.type_string() }}) as message_type_id,
-        updated_at
-
-    from user_unsubscribed_channel
-
-{% if var('iterable__using_user_unsubscribed_message_type', True) %}
-    union all
-
-    select 
-        email,
-        unique_user_key,
-        cast(null as {{ dbt.type_string() }}) as channel_id,
-        message_type_id,
-        updated_at
-    
-    from user_unsubscribed_message_type
-{% endif %}
-
-), final as (
-
-    select
-        combine_histories.email,
-        combine_histories.unique_user_key,
-        -- coalescing since message_type -> channel goes up a grain
-        coalesce(combine_histories.channel_id, message_type_channel.channel_id) as channel_id,
-        coalesce(combine_histories.message_type_id, message_type_channel.message_type_id) as message_type_id,
-        message_type_channel.channel_name,
-        message_type_channel.message_type_name,
-        message_type_channel.channel_type,
-        message_type_channel.message_medium,
-        combine_histories.updated_at,
-        case when combine_histories.channel_id is not null then true else false end as is_unsubscribed_channel_wide
-
-    from combine_histories
-
-    -- unsubscribing from an entire channel unsubscribes a user from all message types in that channel
-    join message_type_channel 
-        on combine_histories.channel_id = message_type_channel.channel_id
-        or combine_histories.message_type_id = message_type_channel.message_type_id
-)
-
-select *
-from final
-
-{% endif %}
