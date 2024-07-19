260cc6a94b173c784c2130dcf7a2b7d63be6dd64
Renee Li <renee.li@fivetran.com>
2024-07-19 15:58:51 -0400

    add validation testing

---
+++ integration_tests/tests/consistency/consistency_user_campaign.sql
@@ -0,0 +1,47 @@
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+-- this test ensures the iterable__user_campaign end model matches the prior version
+with prod as (
+    select *
+    from {{ target.schema }}_iterable_prod.iterable__user_campaign
+    where date(first_event_at) < date({{ dbt.current_timestamp() }})
+),
+
+dev as (
+    select *
+    from {{ target.schema }}_iterable_dev.iterable__user_campaign
+    where date(first_event_at) < date({{ dbt.current_timestamp() }})
+),
+
+prod_not_in_dev as (
+    -- rows from prod not found in dev
+    select * from prod
+    except distinct
+    select * from dev
+),
+
+dev_not_in_prod as (
+    -- rows from dev not found in prod
+    select * from dev
+    except distinct
+    select * from prod
+),
+
+final as (
+    select
+        *,
+        'from prod' as source
+    from prod_not_in_dev
+
+    union all -- union since we only care if rows are produced
+
+    select
+        *,
+        'from dev' as source
+    from dev_not_in_prod
+)
+
+select *
+from final
